b9aed9384c898cfed7d135455c47dd1e9118e8f6
Fernando Gomes <fernandogomescruz@gmail.com>
2023-03-13 17:32:34 -0300

    Ok

---
+++ README.md
@@ -0,0 +1,14 @@
+# att-var4
+Exemplo de média de notas em Python 
+
+## From
+Faculdade de Ciência de Dados.
+
+Matéria: ICD002 Algoritmos e Introdução à Computação
+
+Apostila: Atualização de variáveis + condicionais if e if else - IV
+
+## Environment
+Feito em windows, testado no Linux.
+
+- conda - spyder
---
+++ att_var4.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 10:35:38 2023
+
+@author: fernando.cruz7
+"""
+
+while True:
+    try:
+        nome = input("Digite o nome do Aluno: ")
+        sobrenome = input("Digite o sobrenome do Aluno: ")
+        portugues = float(input("Digite a nota de Português: "))
+        matematica = float(input("Digite a nota de Matemática: "))
+        historia = float(input("Digite a nota de História: "))
+        geografia = float(input("Digite a nota de Geografia: "))
+        educacao_fisica = float(input("Digite a nota de Educação Física: "))
+    except Exception:
+        print("Um dos valores é inválido")
+        continue
+    is_correct = input("As informações estão corretas? Digite \"sim\" para continuar: ").lower()
+    if is_correct in ("yes", "y", "sim", "s"):
+        media = (portugues + matematica + historia + educacao_fisica + geografia) / 5
+        print("")
+        print(f"Nome: {nome}")
+        print(f"Sobrenome: {sobrenome}")
+        print(f"Nota de Português: {portugues}")
+        print(f"Nota de Matemática: {matematica}")
+        print(f"Nota de História: {historia}")
+        print(f"Nota de Geografia: {geografia}")
+        print(f"Nota de Educação Física: {educacao_fisica}")
+        print(f"\n\nMedia do Aluno \"{nome} {sobrenome}\" : {media}")
+        if media >= 5.0:
+            print(f"\nAluno \"{nome} {sobrenome}\" foi APROVADO")
+        if media < 5.0:
+            print(f"\nAluno \"{nome} {sobrenome}\" foi REPROVADO")
+        break
+    else:
+        continue
